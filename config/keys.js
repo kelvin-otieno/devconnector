e64474c38d8d9b6e7fd312937a6bbafeb5b26b49
kelvin-otieno <kelvinotieno06@gmail.com>
2019-04-10 10:00:51 +0300

    first commit with all folders

--- config/keys.js
+++ config/keys.js
@@ -1,3 +1,5 @@
 module.exports = {
-  mongoURI: "mongodb://kevo:Iamnumber1@ds253324.mlab.com:53324/kevconnector"
+  mongoURI: "mongodb://kevo:Iamnumber1@ds253324.mlab.com:53324/kevconnector",
+  mongoURILocal: "mongodb://localhost/kevconnector",
+  secretOrKey: "secret"
 };
